9dd561797045bbc79dee42c271239ccedd93a0e5
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-16 17:56:15 -0300

    aula pt 2

---
+++ aulas_pr/pratica02/Main.java
@@ -0,0 +1,13 @@
+package aulas_pr.pratica02;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero = 10;
+
+        if (numero % 2 == 0) {
+            System.out.println("O número é par.");
+        } else {
+            System.out.println("O número é ímpar.");
+        }
+    }
+}
