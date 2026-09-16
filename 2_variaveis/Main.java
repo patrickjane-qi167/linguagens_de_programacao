28a6c019fb95ea8cec1c874f0ca4d84e59bbd64b
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-16 19:46:20 -0300

    variaveis 2

---
+++ 2_variaveis/Main.java
@@ -0,0 +1,30 @@
+public class Main {
+    public static void main(String[] args) {
+        //variaveis
+        String nome = "Professor";
+        String sobrenome = "Brunao";
+        int idade = 67;
+        double altura = 1.67;
+        boolean empregado = true;
+        String telefone = "11990778554";
+        String endereco = "Rodovia Mario Mori";
+        String cep = "182800-000";
+        String cidade = "Tatui";
+        String uf = "São Paulo";
+        double salario_bruto = 5.500;
+        double valor_hora_extra = 15;
+
+        //prints
+        System.out.println("Nome do aluno: " + nome + " " + sobrenome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Empregado: " + empregado);
+        System.out.println("Telefone: " + telefone);
+        System.out.println("Endereco: " + endereco);
+        System.out.println("Cep: " + cep);
+        System.out.println("Cidade: " + cidade);
+        System.out.println("UF: " + uf);
+        System.out.println("Salario bruto: " + salario_bruto);
+        System.out.println("Valor hora extra: " + valor_hora_extra);
+    }
+}
