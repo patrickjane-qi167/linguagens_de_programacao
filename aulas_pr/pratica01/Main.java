62535888571e9a1ea284b5f88557e1f804043389
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-16 17:55:25 -0300

    aula pt 1

---
+++ aulas_pr/pratica01/Main.java
@@ -0,0 +1,14 @@
+package aulas_pr.pratica01;
+
+public class Main {
+    public static void main(String[] args){
+        int numero1 = 10;
+        int numero2 = 20;
+
+        if (numero1 > numero2) {
+            System.out.println("O maior número é: " + numero1);
+        } else {
+            System.out.println("O maior número é: " + numero2);
+        }
+    }
+}
