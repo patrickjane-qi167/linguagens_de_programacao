4ca1e7f6f2127b20d1dbcf0953d4674ed0820776
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-16 17:57:29 -0300

    aula pt 4

---
+++ aulas_pr/pratica04/Main.java
@@ -0,0 +1,17 @@
+package aulas_pr.pratica04;
+
+public class Main {
+    public static void main(String[] args) {
+        int numero1 = 10;
+        int numero2 = 25;
+        int numero3 = 15;
+
+        if (numero1 > numero2 && numero1 > numero3) {
+            System.out.println("O maior número é: " + numero1);
+        } else if (numero2 > numero1 && numero2 > numero3) {
+            System.out.println("O maior número é: " + numero2);
+        } else {
+            System.out.println("O maior número é: " + numero3);
+        }
+    }
+}
