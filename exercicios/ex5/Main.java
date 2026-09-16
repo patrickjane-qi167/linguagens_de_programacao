cb1657ff2d5022ba845e260b539696c9d29922bc
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-16 17:51:35 -0300

    exercicio 5

---
+++ exercicios/ex5/Main.java
@@ -0,0 +1,10 @@
+public class Main{    
+    public static void main(String[] args){
+
+        String[] produtos = {"bala", "garrafa", "lapis", "escova", "mouse"};
+        
+        for (int i = 0; i < produtos.length; i++) {
+            System.out.println(produtos[5]);
+        }
+    }
+}
