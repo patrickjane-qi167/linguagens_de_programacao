8ee9c4a3827248ee507a52fa1bc323c7d7e6db33
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-16 17:53:15 -0300

    exercico 7

---
+++ exercicios/ex7/Main.java
@@ -0,0 +1,25 @@
+public class Main{
+    public static void main(String[] args){
+
+        int[][] numeros = {
+            {2, 7, 5, 4} , 
+            {9, 4, 1, 5} ,
+            {2, 4, 6, 1} ,
+            {7, 2, 4, 5}
+        };
+
+        System.out.println(numeros[2][3]);
+    }
+}
+
+//for(int i = 0; i < 4; i++) {
+// System.out.println(numeros[i][0]);
+//}
+
+//for(int j = 0; j < 4; j++){
+//System.out.println(numeros[i][j]);
+//}
+
+//for(int j = 0; j < 4; j++){
+// numeros[i][j] = 0;
+//}
