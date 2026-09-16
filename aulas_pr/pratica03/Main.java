b00335ad96b3ef6c85a26759de734f64e94dd825
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-16 17:56:49 -0300

    aula pt 3

---
+++ aulas_pr/pratica03/Main.java
@@ -0,0 +1,15 @@
+package aulas_pr.pratica03;
+
+public class Main {
+    public static void main(String[] args) {
+        int nota = 5;
+
+        if (nota >= 6) {
+            System.out.println("Aprovado");
+        } else if (nota >= 4) {
+            System.out.println("Recuperação");
+        } else {
+            System.out.println("Reprovado");
+        }
+    }
+}
