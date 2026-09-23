14bd99b02d2365a147b9a1f3bcb4a1e0129eae2a
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-23 17:22:43 -0300

    aula pt 11

---
+++ aulas_pr/pratica11/Main.java
@@ -0,0 +1,13 @@
+package aulas_pr.pratica11;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numeros = {10, 20, 30, 40, 50};
+
+        for (int i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i]);
+        }
+
+    }
+}
