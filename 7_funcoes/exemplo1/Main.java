edd38d4ad8077a352edba342240f7caf6d34babb
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-23 17:17:07 -0300

    ex1

---
+++ 7_funcoes/exemplo1/Main.java
@@ -0,0 +1,10 @@
+package ex_aulafuncao;
+public class Main {
+    static void bemVindo(){
+        System.out.println("Seja bem-vindo!");
+    }
+    
+    public static void main(String[] args){
+        bemVindo();
+    }
+}
