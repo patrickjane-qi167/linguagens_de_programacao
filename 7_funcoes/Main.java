237bba7fd6176144c7fd67bc9ca44f9463c80c13
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-23 17:20:22 -0300

    alavanca

---
+++ 7_funcoes/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    static void entrar(){
+        System.out.println("Voce entrou na sala.");
+    }
+
+    static void acionar(){
+        System.out.println("Voce acionou a alavanca.");
+    }
+    static void sair(){
+        System.out.println("Voce saiu da sala.");
+    }
+    public static void main(String[] args){
+        entrar();
+        acionar();
+        sair();
+    }
+}
