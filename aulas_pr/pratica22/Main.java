6f6c8e2a2087fe8b70b6a7b6244b5fd2303a90ab
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-23 17:28:21 -0300

    aula pt 22

---
+++ aulas_pr/pratica22/Main.java
@@ -0,0 +1,24 @@
+package aulas_pr.pratica22;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] numeros = {15, 8, 32, 4, 19, 27, 11};
+
+        int maior = numeros[0];
+        int menor = numeros[0];
+
+        for (int numero : numeros) {
+            if (numero > maior) {
+                maior = numero;
+            }
+
+            if (numero < menor) {
+                menor = numero;
+            }
+        }
+
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+    }
+}
