3738d65bad9d6b3ebd20feb4dc309d8352eaa860
patrickjane-qi167 <patrick.fatecgti@gmail.com>
2026-09-23 22:03:36 -0300

    moto

--- 8_orientacao_a_objetos/exercicio1/Moto.java
+++ 8_orientacao_a_objetos/exercicio1/Moto.java
@@ -1,5 +1,14 @@
-package 8_orientacao_a_objetos.exercicio1;
-
 public class Moto {
-    
+    String marca = "";
+    String modelo = "";
+    float combustivel = 0;
+    int cilindradas = 0; 
+
+    public static void ligarMotor(){ 
+        System.out.println("O Motor Ligou!");
+    }
+
+    public static void desligarMotor(){
+        System.out.println("O Motor Desligou!")
+    }
 }
